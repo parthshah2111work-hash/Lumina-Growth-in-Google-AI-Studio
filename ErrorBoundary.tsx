6c88987d9123fdf2e64c64ae121d25b5c4590378
parthshah2111work-hash <parthshah2111work@gmail.com>
2026-05-04 01:41:07 +0530

    Update ErrorBoundary.tsx

--- ErrorBoundary.tsx
+++ ErrorBoundary.tsx
@@ -1,6 +1,6 @@
 import React, { Component, ComponentType, PropsWithChildren } from "react";
 
-import { ErrorFallback, ErrorFallbackProps } from "@/components/ErrorFallback";
+import { ErrorFallback, ErrorFallbackProps } from "@/ErrorFallback";
 
 export type ErrorBoundaryProps = PropsWithChildren<{
   FallbackComponent?: ComponentType<ErrorFallbackProps>;
